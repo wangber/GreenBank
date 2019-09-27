3888610f8dea2037f91b9909d9836888ae05ab2c
wangber <wangber@foxmail.com>
2019-09-27 10:40:42 +0800

    0927

--- VueDjango/apitest/urls.py
+++ VueDjango/apitest/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path,include
-
+from apitest.views import get_user
 urlpatterns = [
-    
+    path('all_user',get_user),
 ]
 
--- VueDjango/apitest/views.py
+++ VueDjango/apitest/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import serializers
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from index.models import MyUser,History
+
+class MyUserjson(serializers.ModelSerializer):
+    class Meta:
+        model = MyUser
+        fields = ['open_id','nickname','location','all_touru','level',]
+#在这里编写一些接口
+#获取用户的信息
+@api_view(['GET'])
+def get_user(request):
+    all_user = MyUser.objects.all()
+    return Response(all_user)
 
 # Create your views here.
